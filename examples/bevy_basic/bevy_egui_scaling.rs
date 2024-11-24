5c9ab3a8e5aab881a85c3724f6ea7a5a5b24b067
NayamiOR <nayamior@outlook.com>
2024-11-24 12:42:09 +0000

    Update examples, add an example of bevy low power mode

--- examples/bevy_basic/bevy_egui_scaling.rs
+++ examples/bevy_basic/bevy_egui_scaling.rs
@@ -15,8 +15,30 @@ fn main() {
 
 // This function is copied from the bevy_egui example
 // Original source: https://github.com/vladbat00/bevy_egui/blob/main/examples/simple.rs
-// Copyright (c) 2021 Vladyslav Batyrenko
 // Licensed under the MIT License: https://github.com/vladbat00/bevy_egui/blob/main/LICENSE
+
+///MIT License
+//
+// Copyright (c) 2021 Vladyslav Batyrenko
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 fn ui_example_system(mut contexts: EguiContexts) {
     egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
         ui.label("world");
---
+++ examples/bevy_basic/bevy_low_power_mode.rs
@@ -0,0 +1,11 @@
+/// Reference link: https://github.com/bevyengine/bevy/blob/main/examples/window/low_power.rs
+use bevy::prelude::*;
+use bevy::winit::WinitSettings;
+
+fn main() {
+    App::new()
+        .insert_resource(WinitSettings::game())
+        // Power-saving reactive rendering for applications.
+        .insert_resource(WinitSettings::desktop_app())
+        .run();
+}
